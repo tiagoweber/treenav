5e112270f212a383a5559a294352265d8896289a
tweber <tiago.oliveira.weber@gmail.com>
2026-03-05 17:28:19 -0300

    CI/CD: added .github/workflows

--- tests/test_maze.py
+++ tests/test_maze.py
@@ -3,24 +3,24 @@ import copy
 import treenav.problems
 
 
-game = treenav.problems.maze()
-#game = maze("./maps/maze_50_50.txt")
+def test_solve_default_map():
+    game = treenav.problems.maze()
 
-game.print_board()
+    #game.print_board()
 
-test_tree = treenav.tree(game,cross_revisit_allowed=False)
+    test_tree = treenav.tree(game,cross_revisit_allowed=False)
 
-# decision
-next_node = test_tree.root_node
-visited_nodes = 0
-while not(game.check()):
-    test_tree.current_node = next_node
-    test_tree.populate_actions_and_children()
+    # decision
+    next_node = test_tree.root_node
+    visited_nodes = 0
+    while not(game.check()):
+        test_tree.current_node = next_node
+        test_tree.populate_actions_and_children()
 
-    next_node = test_tree.navigate_node_astar(test_tree.current_node)    # also removes current node from nodes_to_visit
-    visited_nodes += 1    
-    #test_tree.print_board_from_node(next_node)    
-    print("Visited nodes: %d \t Nodes to visit: %d"%(visited_nodes,len(test_tree.nodes_to_visit)))
+        next_node = test_tree.navigate_node_astar(test_tree.current_node)    # also removes current node from nodes_to_visit
+        visited_nodes += 1    
+        #test_tree.print_board_from_node(next_node)    
+        print("Visited nodes: %d \t Nodes to visit: %d"%(visited_nodes,len(test_tree.nodes_to_visit)))
 
+    assert game.check()
 
-game.print_board_from_node(test_tree,test_tree.current_node)
